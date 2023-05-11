ad13e55c4d32179e0ffafd99a78604b6bfa5eed8
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-05-11 11:15:32 +0530

    Update Maximum Number of Vowels in a Substring of Given Length.java

--- Maximum Number of Vowels in a Substring of Given Length.java
+++ Maximum Number of Vowels in a Substring of Given Length.java
@@ -1,8 +1,5 @@
 1456. Maximum Number of Vowels in a Substring of Given Length
-Medium
-1.9K
-66
-Companies
+
 Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 
 Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
