ce53b6df8329d841fa4d8e0a32b1b25dbf84b942
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-05-11 11:15:54 +0530

    Update Duplicate Zeros.java

--- Duplicate Zeros.java
+++ Duplicate Zeros.java
@@ -1,9 +1,6 @@
 
 1089. Duplicate Zeros
-Easy
-2.3K
-662
-Companies
+
 Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
 
 Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
