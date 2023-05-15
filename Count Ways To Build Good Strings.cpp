0b250c58c2f537d33eb8ca79ef530ef78ddf01a7
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-05-15 19:29:52 +0530

    Update Count Ways To Build Good Strings.cpp

--- Count Ways To Build Good Strings.cpp
+++ Count Ways To Build Good Strings.cpp
@@ -1,4 +1,5 @@
-Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
+Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each 
+step perform either of the following:
 
 Append the character '0' zero times.
 Append the character '1' one times.
@@ -6,7 +7,8 @@ This can be performed any number of times.
 
 A good string is a string constructed by the above process having a length between low and high (inclusive).
 
-Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.
+Return the number of different good strings that can be constructed satisfying these properties. Since the answer can 
+be large, return it modulo 109 + 7.
 
  // Solution 
   
