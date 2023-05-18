b619c52dc47fc24ae1c7b5c244bfdf4940d663cc
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-05-18 15:48:23 +0530

    Create Edit Distance.java

---
+++ Edit Distance.java
@@ -0,0 +1,72 @@
+72. Edit Distance
+# Hard 
+# Google 
+
+
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+ 
+  
+  // Solution 
+  
+   class Solution {
+    
+    public int minDistance(String word1, String word2) {
+        //let's apply bottom up dp like knapsack will do here
+        int m = word1.length();
+        int n = word2.length();
+        int[][]dp = new int[m+1][n+1];
+        for(int i =0;i<m;i++)
+          Arrays.fill(dp[i],Integer.MAX_VALUE);
+        //let's initialize the array
+        for(int j=0;j<=n;j++)
+            dp[0][j]= j;
+        for(int i = 0;i<=m;i++)
+            dp[i][0] =i;
+        //now let's go for the answer hunt by building the entire array
+        for(int i =1;i<=m;i++)
+            for(int j=1;j<=n;j++)
+            {
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j] =dp[i-1][j-1];
+                else
+                {
+                    int min =Math.min(dp[i][j-1],dp[i-1][j]);
+                    dp[i][j] = 1+Math.min(dp[i-1][j-1],min);
+                }
+            }
+       // System.out.println(Arrays.deepToString(dp));
+        return dp[m][n];
+        
+        
+    }  
+        
+    } 
+
+
+ 
