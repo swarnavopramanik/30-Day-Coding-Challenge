a199eaa0a196c5b11247dca998b14c54fc3f4a1c
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-15 22:27:36 +0530

    Create Design HashSet.java

---
+++ Design HashSet.java
@@ -0,0 +1,56 @@
+705. Design HashSet
+
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does 
+not exist in the HashSet, do nothing.
+ 
+
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+
+// Solution 
+
+class MyHashSet {
+    boolean[] arr  ;
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        arr  = new boolean[1000001];
+    }
+    
+    public void add(int i) {
+        arr[i] = true;
+    }
+    
+    public void remove(int i) {
+        arr[i] = false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int i) {
+        return arr[i];
+    }
+}
+
+
